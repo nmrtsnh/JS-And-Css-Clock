e261f5e0ef7a4abb6d0d9c90f0fdc2261627f078
Namrata Sinha <68506888+nmrtsnh@users.noreply.github.com>
2021-08-25 16:57:24 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const secondHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
+}
