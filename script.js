cc846cac2d7b378401dad9cb86e8d4539bca4375
Namrata Sinha <namratasinha26@gmail.com>
2021-08-25 17:28:03 +0200

    add Date

--- script.js
+++ script.js
@@ -4,4 +4,19 @@ const hourHand = document.querySelector(".hour-hand");
 
 function setDate() {
   const now = new Date();
+
+  const seconds = date.getSeconds();
+  const secondDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+  const mins = date.getMinutes();
+  const minuteDegrees = (min / 60) * 360 + (seconds / 60) * 6 + 90;
+  minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+
+  const hour = date.getHours();
+  const hourDegrees = (hour / 12) * 360 + (mins / 60) * 30 + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
+
+setInterval(setDate, 1000);
+setDate();
