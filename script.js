3efec1b1b32a264dc4917a0ac77166d80d99e936
Namrata Sinha <namratasinha26@gmail.com>
2021-08-25 18:14:47 +0200

    update script.js

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ const hourHand = document.querySelector(".hour-hand");
 function setDate() {
   const now = new Date();
 
-  const seconds = date.getSeconds();
+  const seconds = now.getSeconds();
   const secondDegrees = (seconds / 60) * 360 + 90;
   secondHand.style.transform = `rotate(${secondDegrees}deg)`;
 
-  const mins = date.getMinutes();
+  const mins = now.getMinutes();
   const minuteDegrees = (min / 60) * 360 + (seconds / 60) * 6 + 90;
   minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
 
-  const hour = date.getHours();
+  const hour = now.getHours();
   const hourDegrees = (hour / 12) * 360 + (mins / 60) * 30 + 90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
